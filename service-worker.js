797f818bb0c10bd960c7403f7ab242f747227cc3
takemaru4124 <takerouyoshiyasu@gmail.com>
2026-05-13 08:44:37 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,26 @@
-const CACHE_NAME = 'bridge-app-v1';
-const FILES = ['./bridge_inspection_app.html'];
+const CACHE_NAME = 'bridge-app-v2';
+const FILES = [
+  './bridge_inspection_app.html',
+  './css/style.css',
+  './js/pdfjs.min.js',
+  './js/jszip.min.js',
+  './js/app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
